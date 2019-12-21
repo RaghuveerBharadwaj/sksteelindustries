252fdbd3a8fde2b9ea4066239ba0eb67f4dcd3b5
Veer275# <raghuveer.mh@cognitiveclouds.com>
2019-12-22 01:39:46 +0530

    One Line Commit

--- js/custom.js
+++ js/custom.js
@@ -1,32 +1,28 @@
-document.querySelector('#about').classList.add("hide")
-document.querySelector('#portfolio').classList.add("hide")
-document.querySelector('#services').classList.add("hide")
-document.querySelector('#team').classList.add("hide")
-document.querySelector('#skills').classList.add("hide")
-document.querySelector('#facts').classList.add("hide")
-document.querySelector('#clients').classList.add("hide")
-document.querySelector('#testimonials').classList.add("hide")
-$(document).ready(function(){
-  $(this).scrollTop(0);
-})
-hideProd = (val) => {
-  document.querySelector('#about').classList.add("hide")
-  document.querySelector('#portfolio').classList.add("hide")
-  document.querySelector('#services').classList.add("hide")
-  document.querySelector('#team').classList.add("hide")
-  document.querySelector('#skills').classList.add("hide")
-  document.querySelector('#facts').classList.add("hide")
-  document.querySelector('#clients').classList.add("hide")
-  document.querySelector('#testimonials').classList.add("hide")
-  document.querySelector('#contact').classList.add("hide")
+// document.querySelector('#about').classList.add("hide")
+// document.querySelector('#portfolio').classList.add("hide")
+// document.querySelector('#services').classList.add("hide")
+// document.querySelector('#team').classList.add("hide")
+// document.querySelector('#skills').classList.add("hide")
+// document.querySelector('#facts').classList.add("hide")
+// document.querySelector('#clients').classList.add("hide")
+// document.querySelector('#testimonials').classList.add("hide")
+// hideProd = (val) => {
+//   document.querySelector('#about').classList.add("hide")
+//   document.querySelector('#portfolio').classList.add("hide")
+//   document.querySelector('#services').classList.add("hide")
+//   document.querySelector('#team').classList.add("hide")
+//   document.querySelector('#skills').classList.add("hide")
+//   document.querySelector('#facts').classList.add("hide")
+//   document.querySelector('#clients').classList.add("hide")
+//   document.querySelector('#testimonials').classList.add("hide")
+//   document.querySelector('#contact').classList.add("hide")
 
-  document.querySelector(val).classList.remove("hide")
+//   document.querySelector(val).classList.remove("hide")
 
-  if(val === '#services') {
-    document.querySelector('#skills').classList.remove("hide")
-    document.querySelector('#facts').classList.remove("hide")
-    document.querySelector('#clients').classList.remove("hide")
-  }
-  window.location.assign(val)
-}
-
+//   if(val === '#services') {
+//     document.querySelector('#skills').classList.remove("hide")
+//     document.querySelector('#facts').classList.remove("hide")
+//     document.querySelector('#clients').classList.remove("hide")
+//   }
+//   window.location.assign(val)
+// }
